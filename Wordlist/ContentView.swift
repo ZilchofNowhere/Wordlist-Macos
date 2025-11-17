6fe5fa74bf80d55a3920b60097f7efc1577ef090
Arda Eren Kartal <aekmanisa@gmail.com>
2025-11-18 02:10:45 +0300

    Initial Commit

---
+++ Wordlist/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Wordlist
+//
+//  Created by Arda Eren Kartal on 18.11.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Wordlist/WordlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordlistApp.swift
+//  Wordlist
+//
+//  Created by Arda Eren Kartal on 18.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct WordlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
